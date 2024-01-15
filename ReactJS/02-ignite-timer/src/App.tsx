861dd6a6dd421abca4639ca75e6b7c20814be63f
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2024-01-15 17:48:33 -0300

    wip: estruturação da aplicação

---
+++ ReactJS/02-ignite-timer/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/Button";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+}
