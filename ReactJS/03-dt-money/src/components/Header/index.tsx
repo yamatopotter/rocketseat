2943b5c3dbfc802be89cb786e976a606f09e5e34
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2024-01-26 17:33:41 -0300

    wip: Modulo 3 - aula 2 - parte 6

--- ReactJS/03-dt-money/src/components/Header/index.tsx
+++ ReactJS/03-dt-money/src/components/Header/index.tsx
@@ -1,11 +1,22 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
 import logoImg from "../../assets/logo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
 
-export function Header(){
-    return (<HeaderContainer>
-<HeaderContent>
-    <img src={logoImg} />
-    <NewTransactionButton>Nova Transação</NewTransactionButton>
-</HeaderContent>
-        </HeaderContainer>)
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} />
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
+  );
 }
