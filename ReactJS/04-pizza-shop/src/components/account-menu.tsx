4fa5ae38acea2d52d6f26905ed17113d6bc8c0e1
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2024-02-05 13:35:03 -0300

    add: files that were ignored

---
+++ ReactJS/04-pizza-shop/src/components/account-menu.tsx
@@ -0,0 +1,48 @@
+import { Building, ChevronDown, LogOut } from 'lucide-react'
+
+import { Button } from '@/components/ui/button.tsx'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu.tsx'
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex select-none items-center gap-2"
+        >
+          Pizza Shop
+          <ChevronDown />
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Matheus Barreto</span>
+          <span className="text-xs font-normal text-muted-foreground">
+            matheus@test.com
+          </span>
+        </DropdownMenuLabel>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem>
+          <Building className="mr-2 h-4 w-4" />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
--- ReactJS/04-pizza-shop/src/pages/_layouts/app.tsx
+++ ReactJS/04-pizza-shop/src/pages/_layouts/app.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 
+import { Header } from '@/components/header.tsx'
+
 export function AppLayout() {
   return (
-    <div className="grid min-h-screen grid-cols-2">
-      <h1>Cabeçalho</h1>
+    <div className="flex min-h-screen flex-col antialiased">
+      <Header />
 
-      <div>
+      <div className={'flex flex-1 flex-col gap-4 p-8 pt-6'}>
         <Outlet />
       </div>
     </div>
