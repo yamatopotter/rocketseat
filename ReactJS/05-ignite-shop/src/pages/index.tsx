aaf91e30aa050ae80fe51ec9b43c4cb3dee7fafa
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2024-02-29 21:59:28 -0300

    wip: Aula 5 - modulo 2 - parte 4

--- ReactJS/05-ignite-shop/src/pages/index.tsx
+++ ReactJS/05-ignite-shop/src/pages/index.tsx
@@ -1,9 +1,37 @@
-import { Inter } from "next/font/google";
+import {HomeContainer, Product} from "../styles/pages/home";
+import Image from "next/image";
 
-const inter = Inter({ subsets: ["latin"] });
-
+import camiseta1 from '../assets/shirts/1.png';
+import camiseta2 from '../assets/shirts/2.png';
+import camiseta3 from '../assets/shirts/3.png';
+import camiseta4 from '../assets/shirts/4.png';
 export default function Home() {
   return (
-    <h1>Index Page</h1>
+      <HomeContainer>
+            <Product>
+                <Image src={camiseta1} width={520} height={480} alt={''}/>
+                <footer>
+                    <strong>Camiseta 1</strong>
+                    <span>R$ 79,90</span>
+                </footer>
+            </Product>
+
+          <Product>
+              <Image src={camiseta2} width={520} height={480} alt={''}/>
+
+              <footer>
+                  <strong>Camiseta 3</strong>
+                  <span>R$ 79,90</span>
+              </footer>
+          </Product>
+
+          <Product>
+              <Image src={camiseta3} width={520} height={480} alt={''}/>
+              <footer>
+                  <strong>Camiseta 3</strong>
+                  <span>R$ 79,90</span>
+              </footer>
+          </Product>
+      </HomeContainer>
   );
 }
