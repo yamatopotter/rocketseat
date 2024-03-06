6217991a2c398a8309518414f7bdbfb2ecab8d60
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2024-03-05 22:51:38 -0300

    wip: Aula 5 - modulo 4 - parte 2

--- ReactJS/05-ignite-shop/src/pages/product/[id].tsx
+++ ReactJS/05-ignite-shop/src/pages/product/[id].tsx
@@ -1,6 +1,30 @@
 import { useRouter } from 'next/router'
 
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '@/src/styles/pages/product'
+
 export default function Id() {
   const { query } = useRouter()
-  return <h1>Product: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+          ad adipisci commodi delectus doloremque eius excepturi facere harum
+          iste labore minus, nostrum numquam omnis porro quo, quod similique
+          unde vel.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
