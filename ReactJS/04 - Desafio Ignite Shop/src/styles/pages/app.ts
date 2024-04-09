f48b7aaf787d8cecda0cad63821607420db4d4be
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2024-04-08 22:56:06 -0300

    wip: desafio 4

---
+++ ReactJS/04 - Desafio Ignite Shop/src/styles/pages/app.ts
@@ -0,0 +1,26 @@
+import { styled } from '..'
+
+export const Container = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  justifyContent: 'center',
+  minHeight: '100vh',
+})
+
+export const Header = styled('header', {
+  padding: '2rem 0',
+  width: '100%',
+  maxWidth: 1180,
+  margin: '0 auto',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+})
+
+export const CartButton = styled('button', {
+  backgroundColor: '#202024',
+  padding: '0.85rem',
+  border: 0,
+  borderRadius: 6,
+})
